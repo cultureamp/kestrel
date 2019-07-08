4f9ae9ffeffec3ebe7ae0a81fcfa582d0e5b4b2d
William Boxhall <william.boxhall@cultureamp.com>
2019-07-09 09:49:24 +1000

    WIP

--- src/EventSourcingPrototype.kt
+++ src/EventSourcingPrototype.kt
@@ -18,24 +18,24 @@ data class Renamed(val name: String, val locale: Locale, val namedAt: Date) : Su
 data class Deleted(val deletedAt: Date) : SurveyUpdateEvent()
 data class Restored(val restoredAt: Date) : SurveyUpdateEvent()
 
-sealed class SurveyError
+sealed class SurveyError : Error
 object AlreadyRenamed : SurveyError()
 object AlreadyDeleted : SurveyError()
 object NotDeleted : SurveyError()
 
-data class SurveyAggregateRoot(override val uuid: UUID, val name: Map<Locale, String>, val accountId: UUID, val deleted: Boolean = false) : AggregateRoot<SurveyUpdateCommand, SurveyUpdateEvent, SurveyError, SurveyAggregateRoot> {
-    companion object : AggregateRootFactory<SurveyCreationCommand, SurveyCreationEvent, SurveyAggregateRoot> {
-        override fun create(event: SurveyCreationEvent): SurveyAggregateRoot = when (event) {
-            is Created -> with(event) { SurveyAggregateRoot(aggregateId, name, accountId) }
-            is Snapshot -> with(event) { SurveyAggregateRoot(aggregateId, name, accountId, deleted) }
+data class SurveyAggregate(override val aggregateId: UUID, val name: Map<Locale, String>, val accountId: UUID, val deleted: Boolean = false) : Aggregate<SurveyUpdateCommand, SurveyUpdateEvent, SurveyError, SurveyAggregate> {
+    companion object : AggregateConstructor<SurveyCreationCommand, SurveyCreationEvent, SurveyError, SurveyAggregate> {
+        override fun create(event: SurveyCreationEvent): SurveyAggregate = when (event) {
+            is Created -> with(event) { SurveyAggregate(aggregateId, name, accountId) }
+            is Snapshot -> with(event) { SurveyAggregate(aggregateId, name, accountId, deleted) }
         }
 
-        override fun handle(command: SurveyCreationCommand): Result<SurveyCreationEvent, Error> = when (command) {
+        override fun handle(command: SurveyCreationCommand): Result<SurveyCreationEvent, SurveyError> = when (command) {
             is Create -> with(command) { Result.Success(Created(aggregateId, name, accountId, createdAt)) }
         }
     }
 
-    override fun update(event: SurveyUpdateEvent): SurveyAggregateRoot = when (event) {
+    override fun update(event: SurveyUpdateEvent): SurveyAggregate = when (event) {
         is Renamed -> this.copy(name = name.plus(event.locale to event.name))
         is Deleted -> this.copy(deleted = true)
         is Restored -> this.copy(deleted = false)
@@ -57,23 +57,59 @@ data class SurveyAggregateRoot(override val uuid: UUID, val name: Map<Locale, St
     }
 }
 
+sealed class ThingCreationCommand : CreationCommand
+data class CreateThing(override val aggregateId: UUID) : ThingCreationCommand()
+
+sealed class ThingUpdateCommand : UpdateCommand
+data class Tweak(override val aggregateId: UUID, val tweak: String) : ThingUpdateCommand()
+data class Bop(override val aggregateId: UUID) : ThingUpdateCommand()
+
+sealed class ThingCreationEvent : CreationEvent
+data class ThingCreated(override val aggregateId: UUID) : ThingCreationEvent()
+
+sealed class ThingUpdateEvent : UpdateEvent
+data class Tweaked(val tweak: String) : ThingUpdateEvent()
+object Bopped : ThingUpdateEvent()
+
+data class ThingAggregate(override val aggregateId: UUID, val tweaks: List<String> = emptyList(), val bops: List<Bopped> = emptyList()) : Aggregate<ThingUpdateCommand, ThingUpdateEvent, Error, ThingAggregate> {
+    companion object : AggregateConstructor<ThingCreationCommand, ThingCreationEvent, Error, ThingAggregate> {
+        override fun create(event: ThingCreationEvent): ThingAggregate = when(event) {
+            is ThingCreated -> ThingAggregate(event.aggregateId)
+        }
+
+        override fun handle(command: ThingCreationCommand): Result<CreationEvent, Error> = when(command) {
+            is CreateThing -> Result.Success(ThingCreated(command.aggregateId))
+        }
+    }
+
+    override fun update(event: ThingUpdateEvent): ThingAggregate = when(event){
+        is Tweaked -> this.copy(tweaks = tweaks + event.tweak)
+        Bopped -> this.copy(bops = bops + Bopped)
+    }
+
+    override fun handle(command: ThingUpdateCommand): Result<ThingUpdateEvent, Error> = when(command) {
+        is Tweak -> Result.Success(Tweaked(command.tweak))
+        is Bop -> Result.Success(Bopped)
+    }
+}
+
 enum class Locale {
     en, de
 }
 
-interface AggregateRoot<UC: UpdateCommand, UE: UpdateEvent, Error, Self : AggregateRoot<UC, UE, Error, Self>> {
-    val uuid: UUID
+interface Aggregate<UC: UpdateCommand, UE: UpdateEvent, E: Error, Self : Aggregate<UC, UE, E, Self>> {
+    val aggregateId: UUID
     fun update(event: UE): Self
-    fun handle(command: UC): Result<UE, Error>
+    fun handle(command: UC): Result<UE, E>
 }
 
-interface AggregateRootFactory<CC: CreationCommand, CE: CreationEvent, AggregateType> {
+interface AggregateConstructor<CC: CreationCommand, CE: CreationEvent, E: Error, AggregateType> {
     fun create(event: CE): AggregateType
-    fun handle(command: CC): Result<CreationEvent, Error>
+    fun handle(command: CC): Result<CreationEvent, E>
 }
 
 //interface AggregateRootRepository {
-//    fun <UC: UpdateCommand, UE: UpdateEvent, Error, Self : AggregateRoot<UC, UE, Error, Self>> get(aggregateId: UUID): AggregateRoot<UC, UE, Error, AggregateRoot<UC, UE, Error>>
+//    fun <UC: UpdateCommand, UE: UpdateEvent, Error, Self : Aggregate<UC, UE, Error, Self>> get(aggregateId: UUID): Aggregate<UC, UE, Error, Aggregate<UC, UE, Error>>
 //}
 
 interface Command {
@@ -96,13 +132,20 @@ interface CreationEvent : Event {
 
 interface UpdateEvent : Event
 
-class AggregateRootRegistry(val list: List<AggregateRootFactory<out CreationCommand, out CreationEvent, Self: AggregateRoot<UpdateCommand, UpdateEvent, Any, Self>>>) {
-    fun <C : CreationCommand, E: CreationEvent>aggregateRootFactoryFor(command: C): AggregateRootFactory<C>
+interface Error
+
+class AggregateRootRegistry(val list: List<AggregateConstructor<out CreationCommand, out CreationEvent, out Error, out Aggregate<out UpdateCommand, out UpdateEvent, out Error, *>>>) {
+    fun <CC : CreationCommand>aggregateRootConstructorFor(creationCommand: CC): AggregateConstructor<CC, *, *, *>? {
+        return null
+    }
 }
 
 fun appWiring() {
-    val factory: AggregateRootFactory<SurveyCreationCommand, SurveyCreationEvent, SurveyAggregateRoot> = SurveyAggregateRoot.Companion
-    val aggregateRootRegistry = AggregateRootRegistry(listOf(factory))
+    val first: AggregateConstructor<SurveyCreationCommand, SurveyCreationEvent, SurveyError, SurveyAggregate> = SurveyAggregate.Companion
+    val second: AggregateConstructor<ThingCreationCommand, ThingCreationEvent, Error, ThingAggregate> = ThingAggregate.Companion
+    val constructors = listOf(first, second)
+    val aggregateRootRegistry = AggregateRootRegistry(constructors)
+    val aggregateRootConstructor = aggregateRootRegistry.aggregateRootConstructorFor(CreateThing(UUID.randomUUID()))
 }
 
 sealed class Result<out V, out E> {
