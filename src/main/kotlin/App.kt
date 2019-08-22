16871c4aacadc24160ecdc43adbfca8f28c8fb5c
William Boxhall <william.boxhall@cultureamp.com>
2019-08-22 10:34:08 +0200

    Return BadData payload for malformed input json

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.core.json.ReaderBasedJsonParser
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
 import eventsourcing.Command
 import eventsourcing.CommandController
 import eventsourcing.CommandGateway
@@ -8,6 +11,7 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.http.HttpStatusCode
 import io.ktor.jackson.jackson
 import io.ktor.request.receive
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.post
 import io.ktor.routing.routing
@@ -36,15 +40,26 @@ fun main() {
                     val statusCode = if (commandController.handle(command)) HttpStatusCode.Created else HttpStatusCode.InternalServerError
                     eventStore.eventsFor(command.aggregateId)
                     call.respondText(status = statusCode, text = commandClassName)
+                } catch (e: MismatchedInputException) {
+                    val field = e.path.first().fieldName
+                    val value = (e.processor as ReaderBasedJsonParser).text
+                    call.respond(status = HttpStatusCode.BadRequest, message = BadData(field, value))
+                } catch (e: MissingKotlinParameterException) {
+                    val field = e.path.first().fieldName
+                    call.respond(status = HttpStatusCode.BadRequest, message = BadData(field, null))
                 } catch (e: Exception) {
-                    val sw = StringWriter()
-                    val pw = PrintWriter(sw)
-                    e.printStackTrace(pw)
-                    val sStackTrace = sw.toString() // stack trace as a string
-                    print(sStackTrace)
-                    call.respondText(status = HttpStatusCode.InternalServerError, text = sStackTrace)
+                    logStacktrace(e)
+                    call.respondText(status = HttpStatusCode.InternalServerError, text = "Something went wrong")
                 }
             }
         }
     }.start(wait = true)
+}
+
+data class BadData(val field: String, val invalidValue: String?)
+
+fun logStacktrace(e: Exception) {
+    val sw = StringWriter()
+    e.printStackTrace(PrintWriter(sw))
+    print(sw.toString())
 }
