e11cea2cf50fdb09504e32a695506a0c541e4da8
William Boxhall <william.boxhall@cultureamp.com>
2020-03-20 17:06:38 +1100

    Introduce AsyncEventProcessor to run async projectors in separate threads

---
+++ framework/src/main/kotlin/com/cultureamp/eventsourcing/AsyncEventProcessor.kt
@@ -0,0 +1,35 @@
+package com.cultureamp.eventsourcing
+
+import com.cultureamp.common.Action
+import com.cultureamp.common.ExponentialBackoff
+
+class AsyncEventProcessor(
+    private val eventStore: EventStore,
+    private val bookmarkStore: BookmarkStore,
+    private val bookmarkName: String,
+    private val eventListener: EventListener,
+    private val batchSize: Int = 1000
+) {
+
+    fun run() {
+        ExponentialBackoff(onFailure = { error, consecutiveFailures -> error.printStackTrace() }).run(::projectOneBatch)
+    }
+
+    private fun projectOneBatch(): Action {
+        val bookmark = bookmarkStore.findOrCreate(bookmarkName)
+
+        System.out.println("Polling for events for ${bookmarkName} from ${bookmark.sequence}")
+
+        val (count, sequence) = eventStore.getAfter(bookmark.sequence, batchSize).foldIndexed(
+            0 to bookmark.sequence
+        ) { index, acc, sequencedEvent ->
+            eventListener.handle(sequencedEvent.event)
+            index + 1 to sequencedEvent.sequence
+        }
+        bookmarkStore.save(bookmarkName, bookmark.copy(sequence = sequence))
+
+        System.out.println("Processed ${count} events for ${bookmarkName}")
+
+        return if (count >= batchSize) Action.Continue else Action.Wait
+    }
+}
