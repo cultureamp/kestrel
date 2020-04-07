8c1b22bb9342c77f930be9befaf6ac8751369bed
andy.mackinlay <andy.mackinlay@cultureamp.com>
2020-04-07 12:41:02 +1000

    added some test setup and basic passing test cases

--- src/test/kotlin/com/cultureamp/eventsourcing/CommandGatewayIntegrationTest.kt
+++ src/test/kotlin/com/cultureamp/eventsourcing/CommandGatewayIntegrationTest.kt
@@ -1,105 +1,77 @@
 package com.cultureamp.eventsourcing
 
+import com.cultureamp.eventsourcing.sample.*
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.count
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
-enum class PizzaTopping {
-    CHEESE,
-    TOMATO_PASTE,
-    BASIL,
-    HAM,
-    PINEAPPLE
-}
-
-enum class PizzaStyle {
-    MARGHERITA,
-    HAWAIIAN
-}
-
-sealed class PizzaCommand : Command
-
-sealed class PizzaCreationCommand : PizzaCommand(), CreationCommand
-data class CreateClassicPizza(val baseStyle: PizzaStyle, override val aggregateId: UUID) : PizzaCreationCommand()
-sealed class PizzaUpdateCommand : PizzaCommand(), UpdateCommand
-data class AddTopping(override val aggregateId: UUID, val topping: PizzaTopping) : PizzaUpdateCommand()
-data class EatPizza(override val aggregateId: UUID) : PizzaUpdateCommand()
-
-sealed class PizzaEvent : DomainEvent
-data class PizzaCreated(val baseStyle: PizzaStyle, val initialToppings: List<PizzaTopping>) : PizzaEvent(),
-    CreationEvent
-
-sealed class PizzaUpdateEvent : PizzaEvent(), UpdateEvent
-data class PizzaToppingAdded(val newTopping: PizzaTopping) : PizzaUpdateEvent()
-class PizzaEaten : PizzaUpdateEvent()
-
-sealed class PizzaError : CommandError
-class ToppingAlreadyPresent : PizzaError()
-class PizzaAlreadyEaten : PizzaError()
-
-data class PizzaAggregate(
-    val baseStyle: PizzaStyle, val toppings: List<PizzaTopping>, val isEaten: Boolean = false
-) : Aggregate {
-    constructor(event: PizzaCreated) : this(baseStyle = event.baseStyle, toppings = event.initialToppings)
-
-    fun updated(event: PizzaUpdateEvent): PizzaAggregate = when (event) {
-        is PizzaToppingAdded -> this.copy(toppings = this.toppings + event.newTopping)
-        is PizzaEaten -> this.copy(isEaten = true)
-    }
-
-    companion object {
-        private fun classicToppings(style: PizzaStyle): List<PizzaTopping> = when (style) {
-            PizzaStyle.MARGHERITA -> listOf(PizzaTopping.CHEESE, PizzaTopping.TOMATO_PASTE, PizzaTopping.BASIL)
-            PizzaStyle.HAWAIIAN -> listOf(
-                PizzaTopping.CHEESE,
-                PizzaTopping.TOMATO_PASTE,
-                PizzaTopping.HAM,
-                PizzaTopping.PINEAPPLE
-            )
-        }
-
-
-        fun create(command: PizzaCreationCommand): Either<PizzaError, PizzaCreated> = when (command) {
-            is CreateClassicPizza -> {
-                val initialToppings = classicToppings(command.baseStyle)
-                Right(PizzaCreated(command.baseStyle, initialToppings))
-            }
-        }
-    }
-
-    fun update(command: PizzaUpdateCommand): Either<PizzaError, List<PizzaUpdateEvent>> = when (command) {
-        is AddTopping -> when (toppings.contains(command.topping)) {
-            true -> Left(ToppingAlreadyPresent())
-            false -> Right(listOf(PizzaToppingAdded(command.topping)))
-        }
-        is EatPizza -> when (isEaten) {
-            true -> Left(PizzaAlreadyEaten())
-            false -> Right(listOf(PizzaEaten()))
-        }
-    }
-
-}
+class EmptyMetadata(): EventMetadata()
 
 class CommandGatewayIntegrationTest : DescribeSpec({
     val h2DbUrl = "jdbc:h2:mem:test;MODE=MySQL;DB_CLOSE_DELAY=-1;"
     val h2Driver = "org.h2.Driver"
     val db = Database.connect(url = h2DbUrl, driver = h2Driver)
+    val eventsTable = H2DatabaseEventStore.eventsTable()
     val eventStore = RelationalDatabaseEventStore.create(db)
     val registry = listOf(
-        Configuration.from(
-            PizzaAggregate.Companion::create,
-            PizzaAggregate::update,
-            {evt: PizzaCreated -> PizzaAggregate(evt)},
-            PizzaAggregate::updated
-        )
+        Configuration.from(PizzaAggregate)
     )
+    val gateway = CommandGateway(eventStore, registry)
+
+    afterTest {
+        transaction(db) {
+            SchemaUtils.drop(eventsTable)
+        }
+    }
+
+    beforeTest {
+        eventStore.setUp()
+    }
 
 
     describe("CommandGateway") {
-        it("sets and retrieves a bookmark") {
+        it("accepts a creation event") {
+            val result = gateway.dispatch(CreateClassicPizza(PizzaStyle.MARGHERITA, UUID.randomUUID()), EmptyMetadata())
+            result shouldBe Right(Created)
+            transaction (db) {
+                eventsTable.selectAll().count() shouldBe 1
+            }
         }
 
-        it("returns zero for an unknown bookmark") {
+        it("fails on creation with duplicate UUIDs") {
+            val aggregateId = UUID.randomUUID()
+            val result = gateway.dispatch(CreateClassicPizza(PizzaStyle.MARGHERITA, aggregateId), EmptyMetadata())
+            result shouldBe Right(Created)
+            val result2 = gateway.dispatch(CreateClassicPizza(PizzaStyle.HAWAIIAN, aggregateId), EmptyMetadata())
+            result2 shouldBe Left(AggregateAlreadyExists)
+            transaction (db) {
+                eventsTable.selectAll().count() shouldBe 1
+            }
+        }
+
+        it("accepts a creation then update event") {
+            val aggregateId = UUID.randomUUID()
+            val result = gateway.dispatch(CreateClassicPizza(PizzaStyle.MARGHERITA, aggregateId), EmptyMetadata())
+            result shouldBe Right(Created)
+            val result2 = gateway.dispatch(AddTopping(aggregateId, PizzaTopping.PINEAPPLE), EmptyMetadata())
+            result2 shouldBe Right(Updated)
+            transaction (db) {
+                eventsTable.selectAll().count() shouldBe 2
+            }
+        }
+
+        it("fails on updating with unknown UUID") {
+            val aggregateId = UUID.randomUUID()
+            val result1 = gateway.dispatch(AddTopping(aggregateId, PizzaTopping.PINEAPPLE), EmptyMetadata())
+            result1 shouldBe Left(AggregateNotFound)
+            transaction (db) {
+                eventsTable.selectAll().count() shouldBe 0
+            }
         }
     }
 })
---
+++ src/test/kotlin/com/cultureamp/eventsourcing/sample.kt
@@ -0,0 +1,94 @@
+package com.cultureamp.eventsourcing.sample
+
+import com.cultureamp.eventsourcing.*
+import java.util.*
+
+data class CreateClassicPizza(val baseStyle: PizzaStyle, override val aggregateId: UUID) : PizzaCreationCommand()
+
+enum class PizzaTopping {
+    CHEESE,
+    TOMATO_PASTE,
+    BASIL,
+    HAM,
+    PINEAPPLE
+
+}
+enum class PizzaStyle {
+    MARGHERITA,
+    HAWAIIAN
+
+}
+sealed class PizzaCommand : Command
+
+sealed class PizzaCreationCommand : PizzaCommand(),
+    CreationCommand
+sealed class PizzaUpdateCommand : PizzaCommand(), UpdateCommand
+
+data class AddTopping(override val aggregateId: UUID, val topping: PizzaTopping) : PizzaUpdateCommand()
+data class EatPizza(override val aggregateId: UUID) : PizzaUpdateCommand()
+
+sealed class PizzaEvent : DomainEvent
+
+sealed class PizzaCreationEvent : PizzaEvent(), CreationEvent
+data class PizzaCreated(val baseStyle: PizzaStyle, val initialToppings: List<PizzaTopping>) : PizzaCreationEvent()
+
+sealed class PizzaUpdateEvent : PizzaEvent(), UpdateEvent
+data class PizzaToppingAdded(val newTopping: PizzaTopping) : PizzaUpdateEvent()
+
+class PizzaEaten : PizzaUpdateEvent()
+
+sealed class PizzaError : CommandError
+class ToppingAlreadyPresent : PizzaError()
+class PizzaAlreadyEaten : PizzaError()
+
+data class PizzaAggregate(
+    val baseStyle: PizzaStyle, val toppings: List<PizzaTopping>, val isEaten: Boolean = false
+) : TypedAggregate<PizzaUpdateCommand, PizzaUpdateEvent, PizzaError, PizzaAggregate> {
+    constructor(event: PizzaCreated) : this(baseStyle = event.baseStyle, toppings = event.initialToppings)
+
+    override fun updated(event: PizzaUpdateEvent): PizzaAggregate = when (event) {
+        is PizzaToppingAdded -> this.copy(toppings = this.toppings + event.newTopping)
+        is PizzaEaten -> this.copy(isEaten = true)
+    }
+
+    companion object :
+        AggregateConstructor<PizzaCreationCommand, PizzaCreationEvent, PizzaError, PizzaUpdateCommand, PizzaUpdateEvent, PizzaAggregate> {
+        override fun created(event: PizzaCreationEvent): PizzaAggregate = when (event) {
+            is PizzaCreated -> PizzaAggregate(event)
+        }
+
+        private fun classicToppings(style: PizzaStyle): List<PizzaTopping> = when (style) {
+            PizzaStyle.MARGHERITA -> listOf(PizzaTopping.CHEESE, PizzaTopping.TOMATO_PASTE, PizzaTopping.BASIL)
+            PizzaStyle.HAWAIIAN -> listOf(
+                PizzaTopping.CHEESE,
+                PizzaTopping.TOMATO_PASTE,
+                PizzaTopping.HAM,
+                PizzaTopping.PINEAPPLE
+            )
+        }
+
+
+        override fun create(command: PizzaCreationCommand): Either<PizzaError, PizzaCreated> = when (command) {
+            is CreateClassicPizza -> {
+                val initialToppings = classicToppings(command.baseStyle)
+                Right(
+                    PizzaCreated(
+                        command.baseStyle,
+                        initialToppings
+                    )
+                )
+            }
+        }
+    }
+
+    override fun update(command: PizzaUpdateCommand): Either<PizzaError, List<PizzaUpdateEvent>> = when (command) {
+        is AddTopping -> when (toppings.contains(command.topping)) {
+            true -> Left(ToppingAlreadyPresent())
+            false -> Right(listOf(PizzaToppingAdded(command.topping)))
+        }
+        is EatPizza -> when (isEaten) {
+            true -> Left(PizzaAlreadyEaten())
+            false -> Right(listOf(PizzaEaten()))
+        }
+    }
+}
