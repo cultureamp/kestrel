4dc730b0e7bf312d709cc00e4ff1b5c783379dab
William Boxhall <william.boxhall@cultureamp.com>
2020-05-19 12:49:48 +1000

    Remove unnecessary InMemoryBookmarkStore

--- src/main/kotlin/com/cultureamp/eventsourcing/BookmarkStore.kt
+++ src/main/kotlin/com/cultureamp/eventsourcing/BookmarkStore.kt
@@ -11,16 +11,6 @@ interface BookmarkStore {
     fun save(bookmarkName: String, bookmark: Bookmark)
 }
 
-class InMemoryBookmarkStore : BookmarkStore {
-    private val map = hashMapOf<String, Bookmark>().withDefault { Bookmark(0) }
-
-    override fun bookmarkFor(bookmarkName: String) = map.getValue(bookmarkName)
-
-    override fun save(bookmarkName: String, bookmark: Bookmark) {
-        map[bookmarkName] = bookmark
-    }
-}
-
 class RelationalDatabaseBookmarkStore(val db: Database, val table: Bookmarks = Bookmarks()) : BookmarkStore {
     override fun bookmarkFor(bookmarkName: String): Bookmark = transaction(db) {
         val matchingRows = rowsForBookmark(bookmarkName)
