8e605ab5d094039dc548c8900224844e09065db3
William Boxhall <william.boxhall@cultureamp.com>
2020-09-08 11:17:46 +1000

    Move Aggregate into its own file

---
+++ src/main/kotlin/com/cultureamp/eventsourcing/Aggregate.kt
@@ -0,0 +1,51 @@
+package com.cultureamp.eventsourcing
+
+interface BaseAggregate
+
+interface Aggregate<UC: UpdateCommand, UE: UpdateEvent, Err: CommandError, out Self : Aggregate<UC, UE, Err, Self>> : BaseAggregate {
+    fun updated(event: UE): Self
+    fun update(command: UC): Either<Err, List<UE>>
+    fun aggregateType(): String = this::class.simpleName!!
+}
+
+interface AggregateWithProjection<UC: UpdateCommand, UE: UpdateEvent, Err: CommandError, P, Self : AggregateWithProjection<UC, UE, Err, P, Self>> {
+    fun updated(event: UE): Self
+    fun update(projection: P, command: UC): Either<Err, List<UE>>
+    fun aggregateType(): String = this::class.simpleName!!
+
+    fun partial(projection: P): Aggregate<UC, UE, Err, Aggregate<UC, UE, Err, *>> {
+        return object:Aggregate<UC, UE, Err, Aggregate<UC, UE, Err, *>> {
+
+            override fun updated(event: UE): Aggregate<UC, UE, Err, *> {
+                return this@AggregateWithProjection.updated(event).partial(projection)
+            }
+
+            override fun update(command: UC): Either<Err, List<UE>> {
+                return update(projection, command)
+            }
+
+            override fun aggregateType(): String = this@AggregateWithProjection.aggregateType()
+        }
+    }
+}
+
+interface AggregateConstructor<CC: CreationCommand, CE: CreationEvent, Err: CommandError, UC: UpdateCommand, UE: UpdateEvent, Self: Aggregate<UC, UE, Err, Self>> {
+    fun created(event: CE): Self
+    fun create(command: CC): Either<Err, CE>
+}
+
+interface AggregateConstructorWithProjection<CC: CreationCommand, CE: CreationEvent, Err: CommandError, UC: UpdateCommand, UE: UpdateEvent, P, Self : AggregateWithProjection<UC, UE, Err, P, Self>> {
+    fun created(event: CE): Self
+    fun create(projection: P, command: CC): Either<Err, CE>
+    fun partial(projection: P): AggregateConstructor<CC, CE, Err, UC, UE, Aggregate<UC, UE, Err, *>> {
+        return object:AggregateConstructor<CC, CE, Err, UC, UE, Aggregate<UC, UE, Err, *>> {
+            override fun created(event: CE): Aggregate<UC, UE, Err, *> {
+                return this@AggregateConstructorWithProjection.created(event).partial(projection)
+            }
+
+            override fun create(command: CC): Either<Err, CE> {
+                return create(projection, command)
+            }
+        }
+    }
+}
