859159a16ef58b83d684fb4646825f78287709aa
William Boxhall <william.boxhall@cultureamp.com>
2020-09-16 14:48:44 +1000

    Allow creation of DomainEventProcessor from function handles
    This may seem like unnecessary indirection due to the way we unwrap
    these DomainEventProcessors inside of EventProcessor, however this makes
    us consistent with the way we do something similar over on the write
    (Aggregate and Route) side, i.e. DomainEventProcessors are like
    Aggregates, and EventProcessors are like Routes in that they wrap and
    perform a routing function including the reified type magic it needs to
    do in order to do its job.

--- src/main/kotlin/com/cultureamp/eventsourcing/DomainEventProcessor.kt
+++ src/main/kotlin/com/cultureamp/eventsourcing/DomainEventProcessor.kt
@@ -4,8 +4,28 @@ import java.util.*
 
 interface DomainEventProcessor<E : DomainEvent> {
     fun process(event: E, aggregateId: UUID)
+
+    companion object {
+        fun <E : DomainEvent> from(process: (E, UUID) -> Any?): DomainEventProcessor<E> {
+            return object : DomainEventProcessor<E> {
+                override fun process(event: E, aggregateId: UUID) {
+                    process(event, aggregateId)
+                }
+            }
+        }
+    }
 }
 
 interface DomainEventProcessorWithMetadata<E : DomainEvent, M : EventMetadata> {
     fun process(event: E, aggregateId: UUID, metadata: M, eventId: UUID)
+
+    companion object {
+        fun <E : DomainEvent, M : EventMetadata> from(process: (E, UUID, M, UUID) -> Any?): DomainEventProcessorWithMetadata<E, M> {
+            return object : DomainEventProcessorWithMetadata<E, M> {
+                override fun process(event: E, aggregateId: UUID, metadata: M, eventId: UUID) {
+                    process(event, aggregateId, metadata, eventId)
+                }
+            }
+        }
+    }
 }
--- src/main/kotlin/com/cultureamp/eventsourcing/EventProcessor.kt
+++ src/main/kotlin/com/cultureamp/eventsourcing/EventProcessor.kt
@@ -4,36 +4,37 @@ import com.cultureamp.common.asNestedSealedConcreteClasses
 import java.util.*
 import kotlin.reflect.KClass
 
-data class EventProcessor(val handlers: Map<KClass<DomainEvent>, (DomainEvent, UUID, EventMetadata, UUID) -> Any?>) {
-    val eventClasses = handlers.keys.flatMap { it.asNestedSealedConcreteClasses() }
+class EventProcessor(private val domainEventProcessors: Map<KClass<DomainEvent>, (DomainEvent, UUID, EventMetadata, UUID) -> Any?>) {
+    val eventClasses = domainEventProcessors.keys.flatMap { it.asNestedSealedConcreteClasses() }
 
     fun handle(event: Event) {
-        handlers.filterKeys { it.isInstance(event.domainEvent) }.values.forEach { it(event.domainEvent, event.aggregateId, event.metadata, event.id) }
+        domainEventProcessors.filterKeys { it.isInstance(event.domainEvent) }.values.forEach { it(event.domainEvent, event.aggregateId, event.metadata, event.id) }
     }
 
     @Suppress("UNCHECKED_CAST")
     companion object {
-        inline fun <reified E : DomainEvent> from(noinline handle: (E, UUID) -> Any?): EventProcessor {
-            val ignoreMetadataHandle = { domainEvent: E, aggregateId: UUID, _: EventMetadata, _: UUID -> handle(domainEvent, aggregateId) }
+        inline fun <reified E : DomainEvent> from(noinline process: (E, UUID) -> Any?): EventProcessor {
+            return from(DomainEventProcessor.from(process))
+        }
+
+        inline fun <reified E : DomainEvent, reified M : EventMetadata> from(noinline process: (E, UUID, M, UUID) -> Any?): EventProcessor {
+            return from(DomainEventProcessorWithMetadata.from(process))
+        }
+
+        inline fun <reified E : DomainEvent> from(domainEventProcessor: DomainEventProcessor<E>): EventProcessor {
+            val ignoreMetadataHandle = { domainEvent: E, aggregateId: UUID, _: EventMetadata, _: UUID -> domainEventProcessor.process(domainEvent, aggregateId) }
             val handler = (E::class to ignoreMetadataHandle) as Pair<KClass<DomainEvent>, (DomainEvent, UUID, EventMetadata, UUID) -> Any?>
             return EventProcessor(mapOf(handler))
         }
 
-        inline fun <reified E : DomainEvent, reified M : EventMetadata> from(noinline handle: (E, UUID, M, UUID) -> Any?): EventProcessor {
+        inline fun <reified E : DomainEvent, reified M : EventMetadata> from(domainEventProcessor: DomainEventProcessorWithMetadata<E, M>): EventProcessor {
+            val handle = { domainEvent: E, aggregateId: UUID, metadata: M, eventId: UUID -> domainEventProcessor.process(domainEvent, aggregateId, metadata, eventId) }
             val handler = (E::class to handle) as Pair<KClass<DomainEvent>, (DomainEvent, UUID, EventMetadata, UUID) -> Any?>
             return EventProcessor(mapOf(handler))
         }
 
-        inline fun <reified E : DomainEvent> from(domainEventProcessor: DomainEventProcessor<E>): EventProcessor {
-            return from(domainEventProcessor::process)
-        }
-
-        inline fun <reified E : DomainEvent, reified M : EventMetadata> from(domainEventProcessor: DomainEventProcessorWithMetadata<E, M>): EventProcessor {
-            return from(domainEventProcessor::process)
-        }
-
         fun compose(first: EventProcessor, second: EventProcessor): EventProcessor {
-            return EventProcessor(first.handlers + second.handlers)
+            return EventProcessor(first.domainEventProcessors + second.domainEventProcessors)
         }
     }
 }
