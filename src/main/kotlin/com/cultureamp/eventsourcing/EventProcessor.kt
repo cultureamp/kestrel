67932706f1305c1b64a7ecb84e8ac2039b912815
david.wheeler <david.wheeler@cultureamp.com>
2022-06-21 10:09:58 +1000

    Change EventProcessor.compose to be additive rather than selective - that is, if two entries can handle an event then both will be called, rather than just one of them

--- src/main/kotlin/com/cultureamp/eventsourcing/EventProcessor.kt
+++ src/main/kotlin/com/cultureamp/eventsourcing/EventProcessor.kt
@@ -22,13 +22,13 @@ interface EventProcessor<in M : EventMetadata> {
     fun domainEventClasses(): List<KClass<out DomainEvent>> = emptyList()
 }
 
-class CompositeDomainEventProcessor<in M : EventMetadata> (private val domainEventProcessors: Map<KClass<DomainEvent>, (DomainEvent, UUID, M, UUID) -> Any?>) : EventProcessor<M> {
-    val eventClasses = domainEventProcessors.keys.flatMap { it.asNestedSealedConcreteClasses() }
+class CompositeDomainEventProcessor<in M : EventMetadata> (private val domainEventProcessors: List<Pair<KClass<DomainEvent>, (DomainEvent, UUID, M, UUID) -> Any?>>) : EventProcessor<M> {
+    val eventClasses = domainEventProcessors.flatMap { it.first.asNestedSealedConcreteClasses() }
 
     override fun domainEventClasses() = eventClasses
 
     override fun process(event: Event<out M>) {
-        domainEventProcessors.filterKeys { it.isInstance(event.domainEvent) }.values.forEach { it(event.domainEvent, event.aggregateId, event.metadata, event.id) }
+        domainEventProcessors.filter { it.first.isInstance(event.domainEvent) }.forEach { it.second(event.domainEvent, event.aggregateId, event.metadata, event.id) }
     }
 
     @Suppress("UNCHECKED_CAST")
@@ -44,13 +44,13 @@ class CompositeDomainEventProcessor<in M : EventMetadata> (private val domainEve
         inline fun <reified E : DomainEvent> from(domainEventProcessor: DomainEventProcessor<E>): CompositeDomainEventProcessor<EventMetadata> {
             val ignoreMetadataHandle = { domainEvent: E, aggregateId: UUID, _: EventMetadata, _: UUID -> domainEventProcessor.process(domainEvent, aggregateId) }
             val handler = (E::class to ignoreMetadataHandle) as Pair<KClass<DomainEvent>, (DomainEvent, UUID, EventMetadata, UUID) -> Any?>
-            return CompositeDomainEventProcessor(mapOf(handler))
+            return CompositeDomainEventProcessor(listOf(handler))
         }
 
         inline fun <reified E : DomainEvent, reified M : EventMetadata> from(domainEventProcessor: DomainEventProcessorWithMetadata<E, M>): CompositeDomainEventProcessor<M> {
             val handle = { domainEvent: E, aggregateId: UUID, metadata: M, eventId: UUID -> domainEventProcessor.process(domainEvent, aggregateId, metadata, eventId) }
             val handler = (E::class to handle) as Pair<KClass<DomainEvent>, (DomainEvent, UUID, EventMetadata, UUID) -> Any?>
-            return CompositeDomainEventProcessor(mapOf(handler))
+            return CompositeDomainEventProcessor(listOf(handler))
         }
 
         fun <M : EventMetadata> compose(first: CompositeDomainEventProcessor<M>, second: CompositeDomainEventProcessor<M>): CompositeDomainEventProcessor<M> {
