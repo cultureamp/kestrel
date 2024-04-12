3a22a8957e172c7afc783c2e16ad0b31001fb00a
William Boxhall <william.boxhall@cultureamp.com>
2024-04-12 15:46:01 +1000

    Test fixes for JVM 17

--- src/test/kotlin/com/cultureamp/eventsourcing/example/ParticipantProjector.kt
+++ src/test/kotlin/com/cultureamp/eventsourcing/example/ParticipantProjector.kt
@@ -25,7 +25,7 @@ class ParticipantProjector(private val database: Database): DomainEventProcessor
             is Reinvited -> ParticipantTable.update({ invitationId eq aggregateId }) {
                 it[invitedAt] = event.reinvitedAt
             }
-            is Rereinvited -> throw RuntimeException("Should have been upcasted")
+            is Rereinvited -> throw RuntimeException("Projector only supports upcasted events")
         }
     }
 
